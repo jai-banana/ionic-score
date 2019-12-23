5716da36feb8c5b17e51aae5c9ba248307c6ec02
jai-banana <jailani.banana@gmail.com>
2019-12-24 04:22:18 +0530

    toss2

--- src/app/pages/toss/toss.page.ts
+++ src/app/pages/toss/toss.page.ts
@@ -11,22 +11,40 @@ export class TossPage implements OnInit {
 
   selectedteams: any;
   TeamNotSelectedYet: boolean = true;
+  firstBatting: any;
+  firstBowling: any;
 
   constructor(private tl: TeamsListService, private router: Router) { }
 
   ngOnInit() {
     this.tl.selectedteams_s.subscribe(res => {
       this.selectedteams = res;
+      console.log(this.selectedteams);
+    });
+  }
+
+  selectTeam(val){
+    let tlist = this.tl.teamslist;
+    for(let i = 0; i < tlist.length; i++){
+      if(tlist[i].teamname === val){
+        this.firstBatting = tlist[i];
+        break;
+      };
+    }
+    this.getBowlingTeam();
+    this.TeamNotSelectedYet = false;
+  }
+
+  getBowlingTeam(){
+    this.selectedteams.forEach(t => {
+      if(t !== this.firstBatting){
+        this.firstBowling = t;
+      }
     });
   }
 
   startMatch(){
     this.router.navigate(['/scorer']);
-  }
-
-  selectTeam(val){
-    console.log(val);
-    this.TeamNotSelectedYet = false;
   }
 
 }
